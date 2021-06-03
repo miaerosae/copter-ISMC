fcdd10f9e584b187360f2a72c84685f31f256191
miaerosae <sea9712@snu.ac.kr>
2021-06-03 18:28:59 +0900

    ISMC

---
+++ test.py
@@ -0,0 +1,166 @@
+import numpy as np
+from numpy import arcsin, sin, cos
+import matplotlib.pyplot as plt
+
+import fym.logging
+from fym.core import BaseEnv, BaseSystem
+
+from copter import Copter
+from integral_slidingmode import IntegralSMC
+
+
+class Env(BaseEnv):
+    def __init__(self):
+        super().__init__(solver="odeint", max_t=10, dt=5, ode_step_len=100)
+        self.plant = Copter()
+        ic = np.vstack((0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+        pos_des0 = np.vstack((-1, 1, -2))
+        vel_des0 = np.vstack((0, 0, 0))
+        angle_des0 = np.vstack((0, 0, 0))
+        omega_des0 = np.vstack((0, 0, 0))
+        ref0 = np.vstack((pos_des0, vel_des0, angle_des0, omega_des0))
+
+        self.controller = IntegralSMC(self.plant.J,
+                                      self.plant.m,
+                                      self.plant.g,
+                                      self.plant.d,
+                                      ic,
+                                      ref0)
+        self.action = np.vstack((self.plant.m*self.plant.g, 0, 0, 0))
+
+    def step(self):
+        *_, done = self.update()
+        return done
+
+    def control_allocation(self, forces):
+        rotors = np.linalg.pinv(self.plant.mixer.B).dot(forces)
+        rotors = np.clip(rotors, 0, self.plant.rotor_max)
+
+        return rotors
+
+    def get_ref(self, t, x):
+        pos_des = np.vstack((2*cos(t), 2*sin(t), -t))
+        vel_des = np.vstack((-2*sin(t), 2*cos(t), -1))
+        angle_des = np.zeros((3, 1))
+        omega_des = np.zeros((3, 1))
+        ref = np.vstack((pos_des, vel_des, angle_des, omega_des))
+
+        return ref
+
+    def _get_derivs(self, t, x, p):
+        ref = self.get_ref(t, x)
+
+        K = np.array([[25, 20],
+                      [200, 10],
+                      [200, 10],
+                      [25, 10]])
+        Kc = np.vstack((1, 1, 1, 1))
+        PHI = np.vstack([1] * 4)
+
+        forces = self.controller.get_FM(x, ref, p, K, Kc, PHI, t)
+        self.action = forces
+        rotors = self.control_allocation(forces)
+
+        return rotors, ref
+
+    def set_dot(self, t):
+        x = self.plant.state
+        p = self.controller.state
+        rotors, ref = self._get_derivs(t, x, p)
+
+        self.plant.set_dot(t, rotors)
+        self.controller.set_dot(x, ref)
+
+    def logger_callback(self, i, t, y, *args):
+        states = self.observe_dict(y)
+        x_flat = self.plant.observe_vec(y[self.plant.flat_index])
+        p = self.controller.observe_list(y[self.controller.flat_index])
+        rotors, ref = self._get_derivs(t, x_flat, p)
+        return dict(t=t, **states, rotors=rotors, ref=ref)
+
+
+def run():
+    env = Env()
+    env.logger = fym.logging.Logger("data.h5")
+
+    env.reset()
+
+    while True:
+        env.render()
+        done = env.step()
+
+        if done:
+            break
+
+    env.close()
+
+
+def exp1():
+    run()
+
+
+def exp1_plot():
+    data = fym.logging.load("data.h5")
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(4, 1, 1)
+    ax2 = fig.add_subplot(4, 1, 2, sharex=ax1)
+    ax3 = fig.add_subplot(4, 1, 3, sharex=ax1)
+    ax4 = fig.add_subplot(4, 1, 4, sharex=ax1)
+
+    ax1.plot(data['t'], data['plant']['pos'].squeeze(), "k-", label="plant")
+    ax1.plot(data["t"], data["ref"][:, 0, 0], "r--", label="plant (cmd)")
+    ax1.plot(data["t"], data["ref"][:, 1, 0], "r--", label="y (cmd)")
+    ax1.plot(data["t"], data["ref"][:, 2, 0], "r--", label="z (cmd)")
+    ax2.plot(data['t'], data['plant']['vel'].squeeze())
+    ax3.plot(data['t'], data['plant']['angle'].squeeze())
+    ax4.plot(data['t'], data['plant']['omega'].squeeze())
+
+    ax1.set_ylabel('Position [m]')
+    ax1.legend([r'$x$', r'$y$', r'$z$'])
+    ax1.grid(True)
+
+    ax2.set_ylabel('Velocity [m/s]')
+    ax2.legend([r'$u$', r'$v$', r'$w$'])
+    ax2.grid(True)
+
+    ax3.set_ylabel('Euler angle [rad]')
+    ax3.legend([r'$phi$', r'$theta$', r'$psi$'])
+    ax3.grid(True)
+
+    ax4.set_ylabel('Angular Velocity [rad/s]')
+    ax4.legend([r'$p$', r'$q$', r'$r$'])
+    ax4.set_xlabel('Time [sec]')
+    ax4.grid(True)
+
+    plt.tight_layout()
+
+    fig2 = plt.figure()
+    ax1 = fig2.add_subplot(4, 1, 1)
+    ax2 = fig2.add_subplot(4, 1, 2, sharex=ax1)
+    ax3 = fig2.add_subplot(4, 1, 3, sharex=ax1)
+    ax4 = fig2.add_subplot(4, 1, 4, sharex=ax1)
+
+    ax1.plot(data['t'], data['rotors'].squeeze()[:, 0])
+    ax2.plot(data['t'], data['rotors'].squeeze()[:, 1])
+    ax3.plot(data['t'], data['rotors'].squeeze()[:, 2])
+    ax4.plot(data['t'], data['rotors'].squeeze()[:, 3])
+
+    ax1.set_ylabel('rotor1')
+    ax1.grid(True)
+    ax2.set_ylabel('rotor2')
+    ax2.grid(True)
+    ax3.set_ylabel('rotor3')
+    ax3.grid(True)
+    ax4.set_ylabel('rotor4')
+    ax4.grid(True)
+    ax4.set_xlabel('Time [sec]')
+
+    plt.tight_layout()
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    exp1()
+    exp1_plot()
