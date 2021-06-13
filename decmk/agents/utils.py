9260c77a8da9c363a2fe8c9a1c024ce24955a17b
miaerosae <sea9712@snu.ac.kr>
2021-06-13 16:09:51 +0900

    correct LoE, effectiveness matrix, FDI

--- decmk/agents/utils.py
+++ decmk/agents/utils.py
@@ -1,17 +1,6 @@
 import numpy as np
 
 
-class CA():
-    def __init__(self, B):
-        self.B = B
-
-    def get(self, fault_index):
-        self.B[:, fault_index] = np.zeros((4, 1))
-        Bf = self.B
-
-        return Bf
-
-
 class LoE:
     def __init__(self, time=0, index=0, level=1.0):
         self.time = time
@@ -20,13 +9,10 @@ class LoE:
 
     def get(self, t, u):
         effectiveness = self.get_effectiveness(t, u.shape[0])
-        # effectiveness = np.ones_like(u)
-        # if t >= self.time:
-        #     effectiveness[self.index] = self.level
         return u * effectiveness.reshape(4, 1)  # , effectiveness
 
     def get_effectiveness(self, t, n):
-        effectiveness = np.array([1] * n)
+        effectiveness = np.array([1.] * n)
         if t >= self.time:
             effectiveness[self.index] = self.level
         return effectiveness
@@ -40,21 +26,13 @@ class FDI:
         fault_index = np.where(np.diag(W) < 1)[0]
         return fault_index
 
-    # def get_W(self, u, uc):
-    #     uc = np.clip(uc, 0, None)
-    #     w = np.hstack([
-    #         ui/uci if not np.isclose(uci, 0)
-    #         else 1 if (np.isclose(ui, 0) and np.isclose(uci, 0))
-    #         else 0
-    #         for ui, uci in zip(u, uc)])
-    #     self.state = np.diag(w)
-    #     return np.diag(w)
-
 
 if __name__ == "__main__":
     t = 4
     loe = LoE(time=3, index=0, level=0.5)
     loe1 = LoE(time=5, index=1, level=0)
-    ca = CA(np.vstack(([1, 1, 1], [1, 1, 1], [1, 1, 1])))
-    actuator_faults = [loe, loe1]
-    print(ca.get(t, actuator_faults))
+    print(loe.get_effectiveness(t, 4))
+    print(loe.get(t, np.vstack((1, 2, 3, 4))))
+    W = np.diag((1, 0.5, 1, 1))
+    fdi = FDI(4)
+    print(fdi.get_index(W))
